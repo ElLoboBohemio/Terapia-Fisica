87b784715a019db677e40c400a610dc2310b9cdb
Josue L <josuelobbo@gmail.com>
2021-08-04 18:33:57 -0600

    Updates

---
+++ static/js/11.e3877465.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpphysio=this.webpackJsonpphysio||[]).push([[11],{41:function(e,c,i){"use strict";i.r(c);i(0);var t=i.p+"static/media/Bibliografia.c171e62e.pdf",a=i(1);c.default=function(){return Object(a.jsx)(a.Fragment,{children:Object(a.jsx)("div",{className:"referencias-pdf",children:Object(a.jsx)("object",{className:"referencias-pdf__object",data:t,type:"application/pdf",children:Object(a.jsx)("embed",{src:t,type:"application/pdf"})})})})}}}]);
+//# sourceMappingURL=11.e3877465.chunk.js.map
